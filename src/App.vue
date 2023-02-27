99c4aeef520009c8d3d5650f31e729ae472b341d
peterdoolaard <peter.doolaard@xs4all.nl>
2023-02-27 10:04:19 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <!--  component TheHeader-->
+
+    <!--  component SearchField-->
+
+    <!--  component TheResult-->
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
